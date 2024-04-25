4c2cd371632e8268401eff4afd06e17fb75d85f7
Aastha Modi <154409320+AASTHA2020@users.noreply.github.com>
2024-04-25 19:30:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,115 +1,65 @@
+// Get the container where we want to put our Pokémon cards
+let container = document.getElementById("wrapper");
 
+// Define how many Pokémon we want to fetch and where to start
 let limit = 20;
 let offset = 0;
 
-const pikapikaDiv = document.querySelector("#pikapika");
-const loadMoreButton = document.querySelector(".loadMore");
-
-
+// When the webpage loads, do the following:
 window.addEventListener("load", async () => {
-  
-  const data = await getDataFromAPI(
+  // Fetch data about Pokémon from the PokeAPI using the limit and offset
+  const pokemons = await getPokemons(
     "https://pokeapi.co/api/v2/pokemon?limit=" + limit + "&offset=" + offset
   );
- 
-  populatePokemons(data);
-});
 
-function showExtraDiv(e) {
-  const parentDiv = e.target.closest('.parent');
-  if (e.target.innerText === "Know More") {
-    e.target.innerText = "Know Less";
-    e.target.nextElementSibling.style.display = "block";
-    e.target.nextElementSibling.style.opacity = 1;
-    parentDiv.querySelector('.flip-card-inner').classList.add('flipped');
-  } else {
-    e.target.innerText = "Know More";
-    e.target.nextElementSibling.style.display = "none";
-    e.target.nextElementSibling.style.opacity = 0;
-    parentDiv.querySelector('.flip-card-inner').classList.remove('flipped');
-  }
-}
-
-// extra information and flip the card
-const knowMoreButtons = document.querySelectorAll('.know-more');
-knowMoreButtons.forEach(button => {
-  button.addEventListener('click', showExtraDiv);
-});
-
-
-// fetch data from the API
-async function getDataFromAPI(url) {
-  
-  const response = await fetch(url);
-    const result = await response.json();
-    return result;
-}
-
-// populate Pokemon cards
-async function populatePokemons(data) {
-  const promises = data.results.map((pokemon) => getDataFromAPI(pokemon.url));
-  const pokemonData = await Promise.all(promises);
-
-  
-  pokemonData.forEach((pokemon) => {
-     const div = document.createElement("div");
-      div.classList.add("parent");
-
-    const image = document.createElement("img");
-    image.src = pokemon.sprites.other.dream_world.front_default;
-
-    const name = document.createElement("p");
-    name.innerText = pokemon.name;
-
-    const type = document.createElement("p");
-    type.innerText = "Type: " + pokemon.types[0].type.name;
-
-    const knowMore = document.createElement("button");
-    knowMore.innerText = "Know More";
-    knowMore.classList.add("know-more");
-    knowMore.addEventListener("click", showExtraDiv);
-
-    const extraDiv = document.createElement("div");
-    extraDiv.classList.add("extraDiv");
-
-    const height = document.createElement("p");
-    height.innerHTML = "<strong>Height:  </strong>" + pokemon.height + " cm";
-    extraDiv.append(height);
-
-    const weight = document.createElement("p");
-    weight.innerHTML = "<strong>Weight: </strong>" + pokemon.weight + " kg";
-    extraDiv.append(weight);
-
-    pokemon.stats.forEach((stat) => {
-      const para = document.createElement("p");
-      para.classList.add("stat");
-      para.innerHTML =
-        "<strong>" + stat.stat.name + "</strong>" + stat.base_stat;
-      extraDiv.append(para);
-    });
-
-    const backCard = document.createElement("div");
-    backCard.classList.add("back-pokemon-card");
-    backCard.append(extraDiv);
-
-    const frontCard = document.createElement("div");
-    frontCard.classList.add("front-pokemon-card");
-    frontCard.append(image, name, type, knowMore);
-
-    const cardInner = document.createElement("div");
-    cardInner.classList.add("flip-card-inner");
-    cardInner.append(frontCard, backCard);
-
-    div.append(cardInner);
-
-    pikapikaDiv.append(div);
+  // For each Pokémon we fetched, do the following:
+  pokemons.results.forEach(async (pokemon) => {
+    // Fetch more detailed data about this Pokémon using its URL
+    const response = await getPokemons(pokemon.url);
+    // Create a card for this Pokémon using its detailed data
+    createPokemonCard(response);
   });
+});
+
+// Function to fetch data about Pokémon from a given URL
+async function getPokemons(uri) {
+  // Fetch data from the given URL
+  const response = await fetch(uri);
+  // Convert the fetched data into a format we can use (like a list of Pokémon)
+  const result = await response.json();
+  // Return the converted data
+  return result;
 }
 
-loadMoreButton.addEventListener("click", async () => {
-  offset = offset + limit;
-  const response = await getDataFromAPI(
-    "https://pokeapi.co/api/v2/pokemon?limit=" + limit + "&offset=" + offset
-  );
-  populatePokemons(response);
-});
+// Function to create a Pokémon card using its data
+function createPokemonCard(pokemonData) {
+  // Create different parts of the card: a container, a front side, a back side, an image, and a name
+  let cardContainer = document.createElement("div");
+  let cardContent = document.createElement("div");
+  let frontSide = document.createElement("div");
+  let backSide = document.createElement("div");
+  let image = document.createElement("img");
+  let name = document.createElement("p");
+
+  // Set the image source and the Pokémon's name based on the fetched data
+  image.src = pokemonData.sprites.other.dream_world.front_default;
+  name.innerHTML = pokemonData.name;
+
+  // Put the image on the front side of the card and the name on the back side
+  frontSide.appendChild(image);
+  backSide.appendChild(name);
+
+  // Assemble the card structure by putting the front and back sides inside the main container
+  cardContent.appendChild(frontSide);
+  cardContent.appendChild(backSide);
+  cardContainer.appendChild(cardContent);
+
+  // Add CSS classes to style the card
+  cardContainer.classList.add("flip-card");
+  cardContent.classList.add("flip-card-inner");
+  frontSide.classList.add("flip-card-front");
+  backSide.classList.add("flip-card-back");
+
+  // Add the card to the container on the webpage
+  container.appendChild(cardContainer);
+}
